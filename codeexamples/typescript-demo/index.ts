7ba8cef07c92ded79364c997aa9161153d2d34cd
Sivasankar Thalavai <s2.thalavai@gmail.com>
2026-02-23 17:17:25 +0530

    Update index.ts

--- codeexamples/typescript-demo/index.ts
+++ codeexamples/typescript-demo/index.ts
@@ -1241,40 +1241,168 @@ console.log("------------Generics in TypeScript------------------------------");
 // Since now we do not know the return type of this function we end up usign the return type as
 // any as well. We are back to JS behaviour
 
-//* function returnParam(param: any): any {
-//*   return param;
-//* }
+function returnParam(param: any): any {
+   return param;
+}
 
 // Here, we will use a type variable, a special kind of variable that works on types rather than values. This is a generic variable we can pass to our function to retain the type of the value used
 // This is how it is done in case of functions
 
-function returnParam<Type>(param: Type): Type {
+function returnParam1<Type>(param: Type): Type {
   return param;
 }
 
 // With this generic in place the value of the type used to invoke the function is retained
 // thi is one way of invoking the function
-let stringOutput = returnParam<string>("string");
+let stringOutput = returnParam1<string>("string");
 
 //Here we explicitly set Type to be string as one of the arguments to the function call, denoted using the <> around the arguments rather than ().
 
 // The second way is also perhaps the most common. Here we use type argument inference — that is, we want the compiler to set the value of Type for us automatically based on the type of the argument we pass in:
-let stringOutput2 = returnParam("string");
-let numberOutput = returnParam(100);
-let numberArray = returnParam([1, 2, 3]);
-let objectOutput = returnParam({ name: "Mark", age: 21 });
-
-// Generic function declaration as an arrow function
-// using a call signature
-const myParam: <T>(param: T) => T = (param) => param;
+let stringOutput2 = returnParam1("string");
+let numberOutput = returnParam1(100);
+let numberArray = returnParam1([1, 2, 3]);
+let objectOutput = returnParam1({ name: "Mark", age: 21 });
 
 // Using a function expression
 const myParam2 = function <U>(param: U): U {
   return param;
 };
 
+
+// Generic function declaration as an arrow function
+// using a call signature
+const myParam: <T>(param: T) => T = (param) => param;
+
+
 // Using a call signature in an object
 type ObjectType = {
   myParam: <V>(param: V) => V;
 };
   
+
+console.log("---------Generics with Constraints in an Array ---------------------");
+
+//  First declare a generics function signature
+type GetFirstElement = <T>(arr: T[]) => T;
+// A generic array function that gets first element of every type of array
+const getFirstElement: GetFirstElement = (arr) => {
+  return arr[0];
+};
+
+// We declare two different types of array
+const numberArray = [1, 2, 3];
+const stringArray = ["a", "b", "c"];
+
+// Typescript is correctly able to infer the value that will be return by expression
+// Even though the function is the same the returned type is different based on the input value
+let stringOutput = getFirstElement(stringArray);
+let numberOutput = getFirstElement(numberArray);
+
+// Where do declare generic dictates when typescript will binc a concrete type to a generic
+// what if the above function was declared with a different placement of generic
+type FirstElement<T> = (arr: T[]) => T;
+
+// Here if the generic type is not passed at the time of function declaration TS will throw and error
+// So now you need to tell TypeScript which types can we used with this fucntion which solves a completely different purpose from the function that has been declared above
+// Hover over the function param and you will see that TS now identifies that param will be an
+// array of strings
+const firstElement: FirstElement<string> = (arr) => {
+  return arr[0];
+};
+
+// Generics can have constraints as well
+type HasLength = {
+  length: number;
+};
+
+function logLength<T extends HasLength>(item: T): void {
+  console.log(item.length);
+}
+
+// Any array like value that has a length property on it will be accepted as an argument
+logLength(numberArray);
+logLength(stringArray);
+logLength("Any String");
+
+// But if used for an object it will throw an error
+logLength({ name: "John", length: 12 });
+
+--------------------
+
+console.log("polymorphic (generic) version");
+
+// In TypeScript, real polymorphism = Generics
+
+const filter = <T>(array: T[], predicate: (item: T) => boolean): T[] => {
+  let result: T[] = [];
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    if (predicate(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+Now it’s type-safe polymorphic.
+
+Usage:
+
+let numbers = [1, 3, 4, 6, 9, 7, 10, 12];
+let animals = ["cat", "bat", "rat", "mat"];
+
+function predicate(item: number) {
+  return item > 7;
+}
+
+function filterCats(item: string) {
+  return item === "cat";
+}
+
+console.log(filter(numbers, predicate));   // number[]
+console.log(filter(animals, filterCats));  // string[]
+
+
+// ----------------
+
+  type Filter = {
+  (array: number[], predicate: (item: number) => boolean): number[];
+  (array: string[], predicate: (item: string) => boolean): string[];
+  (array: object[], predicate: (item: object) => boolean): object[];
+};
+
+// Trying to create a simple implementation of JavaScript's own filter method
+const filter = (array: any[], predicate: Function) => {
+  let result: any[] = [];
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    if (predicate(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+let numbers = [1, 3, 4, 6, 9, 7, 10, 12];
+// Predicate to filter all numbers greater than 7
+function predicate(item: number) {
+  return item > 7;
+}
+
+let animals = ["cat", "bat", "rat", "mat"];
+// Predicate to filter all cats from animals array
+function filterCats(item: string) {
+  return item === "cat";
+}
+
+// Result of invoking the function
+console.log(filter(numbers, predicate));
+console.log(filter(animals, filterCats));
+
+// -----------
+
+console.log("-----------use generics instead of Function OVerloading-----------------------");
+
+
+
