67bf7f8e2030f201e0dbd7d338843228304f1bb7
Sivasankar Thalavai <s2.thalavai@gmail.com>
2026-02-27 18:49:40 +0530

    Create OddEvenThreadExample.java

---
+++ codeexamples/OddEvenThreadExample.java
@@ -0,0 +1,48 @@
+class NumberPrinter {
+    private int number = 1;
+    private final int MAX = 10;
+
+    public synchronized void printOdd() {
+        while (number <= MAX) {
+            if (number % 2 == 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Odd Thread: " + number);
+                number++;
+                notify();
+            }
+        }
+    }
+
+    public synchronized void printEven() {
+        while (number <= MAX) {
+            if (number % 2 != 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Even Thread: " + number);
+                number++;
+                notify();
+            }
+        }
+    }
+}
+
+public class OddEvenThreadExample {
+    public static void main(String[] args) {
+        NumberPrinter printer = new NumberPrinter();
+
+        Thread t1 = new Thread(() -> printer.printOdd());
+        Thread t2 = new Thread(() -> printer.printEven());
+
+        t1.start();
+        t2.start();
+    }
+}
