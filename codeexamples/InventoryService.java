9371fef5be98e9ca2f9b8f48e6f817f59841d6cb
Sivasankar Thalavai <s2.thalavai@gmail.com>
2026-02-27 11:02:41 +0530

    Create InventoryService.java

---
+++ codeexamples/InventoryService.java
@@ -0,0 +1,28 @@
+// inventory service using ReentrantLock
+
+import java.util.concurrent.locks.ReentrantLock;
+
+class InventoryService {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private int stock = 100;
+
+    public void purchase(int quantity) {
+        lock.lock();
+        try {
+            stock -= quantity;
+            System.out.println("Stock updated: " + stock);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int checkStock() {
+        lock.lock();
+        try {
+            return stock;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
